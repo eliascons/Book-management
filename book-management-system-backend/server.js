e3dea55b3011c8114bdc24cd55c2b5a274f48d7f
Elias Consalvo <econs013@fiu.edu>
2023-05-20 17:26:07 -0400

    updated apollo 3 to 4

--- book-management-system-backend/server.js
+++ book-management-system-backend/server.js
@@ -1,24 +1,36 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import express from 'express';
-import { ApolloServer } from 'apollo-server';
-import typeDefs from './graphql/schema.js';
+import http from 'http';
+import cors from 'cors';
+import  bodyParser  from 'body-parser';
 import resolvers from './graphql/resolvers/resolvers.js';
+import typeDefs from './graphql/schema.js';
 
 const app = express();
-const PORT = 3000; // Replace with your desired port number
+const httpServer = http.createServer(app);
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   context: ({ req }) => {
     const token = req.headers.authorization || "";
     return { token };
   },
 });
 
-server.listen().then(({ url }) => {
-    console.log(`Server is running at ${url}`);
-  });
+await server.start();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use(
+  '/graphql',
+  cors(),
+  bodyParser.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  }),
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(` Server ready at http://localhost:4000/graphql`);
